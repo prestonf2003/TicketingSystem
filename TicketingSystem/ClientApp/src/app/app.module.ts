2ea1fdc87f10fe94c41cc3d5794e4ead2c0de391
Nicholas Wierenga <NicholasWierenga@gmail.com>
2022-06-12 22:58:53 -0400

    deleted unused components(home, counter, fetch-data) and weather controller

--- TicketingSystem/ClientApp/src/app/app.module.ts
+++ TicketingSystem/ClientApp/src/app/app.module.ts
@@ -6,9 +6,6 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { TodoDisplayComponent } from './todo-display/todo-display.component';
 import { TicketViewComponent} from './ticket-view/ticket-view.component';
 import { SubmitTicketComponent } from './submit-ticket/submit-ticket.component';
@@ -18,9 +15,6 @@ import { StatsComponent } from './stats/stats.component';
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     TodoDisplayComponent,
     TicketViewComponent,
     SubmitTicketComponent,
@@ -32,8 +26,6 @@ import { StatsComponent } from './stats/stats.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: TodoDisplayComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'todo-display', component: TodoDisplayComponent },
       { path: 'submit-ticket', component: SubmitTicketComponent },
       { path: 'ticket-view', component: TicketViewComponent },
